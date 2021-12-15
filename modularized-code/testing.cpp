b90087f832f696991cbc47420106ac09e117caeb
jmartinezar <jmartinezar@unal.edu.co>
2021-12-15 01:52:04 -0500

    arreglando los 4 cuadros

--- modularized-code/testing.cpp
+++ modularized-code/testing.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <fstream>
 
-int main(int argc, char ** argv)
+int main(int argc, char * argv)
 {
     int Nmol = 0;
     int size = 0;
@@ -23,17 +23,17 @@ int main(int argc, char ** argv)
 
     //Ubica las particulas aleatoriamente en su distribucion inicial
     //cerca al centro de la taza
-    initial_distribution_array(Nmol, size, init_size, particles, seed);
+    cuatro_cuadros_centrados(Nmol, size, init_size, particles, seed);
 
     std::cout << "Para una copa de arista par, el radio de la distribución inicial debe ser raíz de 2 sobre 2, a continuación se imprimen aristas pares con el error de su radio respecto al valor esperado\n";
 
     for(int ii = 0; ii < 20; ++ii)
     {
-        size += ii*2;
         std::cout << size << "\t" << std::fabs(std::sqrt(2)*radius(Nmol, particles, size) - 1) << "\n";
         particles = {0};
         particles.resize(Nmol);
-        initial_distribution_array(Nmol, size, init_size, particles, seed);
+        size += 2;
+        cuatro_cuadros_centrados(Nmol, size, init_size, particles, seed);
     }
 
     return 0;
